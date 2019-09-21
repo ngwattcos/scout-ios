cb9ffa537f4ac23245041bbb7e0b0c97d56c5be1
ngwattcos <sw754@cornell.edu>
2019-09-21 16:37:56 -0400

    initial commit

---
+++ Scout/Scout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scout
+//
+//  Created by Scott Wang on 9/21/19.
+//  Copyright © 2019 Scott Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
